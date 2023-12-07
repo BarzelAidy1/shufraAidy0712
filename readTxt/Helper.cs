6830b2f36321a33db8adbaad1266884a4d26729b
aidybarzel <aidybarzel@gmail.com>
2023-12-07 09:42:52 +0200

    my commit 1

---
+++ readTxt/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readTxt
+{
+    public class Helper
+    {
+        private readonly static Helper _instance=new Helper();
+
+        public static Helper Instance => _instance;
+
+        public string[]MessagesList { get; set; }
+        private Helper()
+        {
+            //טעינת הודעות מקובץ
+        }
+
+    }
+}
---
+++ readTxt/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using readTxt;
+
+var txt = File.ReadAllText("txt.txt");
+Console.WriteLine(txt);
+Console.ReadLine();
+
+//singleton
+
+foreach (var message in Helper.Instance.MessagesList)
+{
+    Console.WriteLine(message);
+
+}
